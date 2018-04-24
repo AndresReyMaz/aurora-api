a2e3281cbb678078b17a6e79a379020fdd1da289
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-04-24 00:52:45 -0500

    Edited delete booking

--- config/cron.js
+++ config/cron.js
@@ -4,6 +4,7 @@ module.exports.cron = {
   startAnnoy: {
     schedule: '25,55 7-19 * * 1-5',
     onTick: async function() {
+      sails.log((new Date()) + '-- CRON: startAnnoy');
       // Get all the rooms which are still booked
       let rooms = await Rooms.find({ inUse: true });
       if (rooms === undefined) {
